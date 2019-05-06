12d2f6331be196e26f65d492bf99fa3d2f3176f2
Mohammed Ghoname <MohamedSalehGhoname2@gmail.com>
2019-05-06 17:11:12 +0200

    Fixing keys

--- webclient/src/app/services/servecisRoutes.ts
+++ webclient/src/app/services/servecisRoutes.ts
@@ -2,5 +2,5 @@ export class ServiceRoutes {
   static CustomersService_getCustomers= 'api/customers/';
   static PingService_simulateOff = 'api/vichlesping/off/';
   static PingService_ping = 'api/vichlesping/';
-  static ViechleStatus = 'api/vichlesstatus/';
+  static ViechleStatus = 'api/vichlesping/status';
 }
