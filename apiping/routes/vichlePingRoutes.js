d62d2870fa563ed4638465004a52798c33183c5b
Mohammed Ghoname <MohamedSalehGhoname2@gmail.com>
2019-05-06 20:54:08 +0200

    Fixing keys

--- apiping/routes/vichlePingRoutes.js
+++ apiping/routes/vichlePingRoutes.js
@@ -9,6 +9,6 @@ router.use((req, res, next) =>{ next(); });
 router.get('/test', (req, res) => {res.send('Ping router works');});
 router.post('/',vichlesPingController.ping);
 router.post('/off',vichlesPingController.simulateOFF);
-router.post('/status',vichlesPingController.getStatuses);
+router.get('/status',vichlesPingController.getStatuses);
 
 module.exports=router;
